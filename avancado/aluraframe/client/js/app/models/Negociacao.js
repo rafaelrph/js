9433744a9988e176497c0a85edd971a540c1d6db
Rafael Barros <rafael.rph7@gmail.com>
2017-08-14 20:48:43 -0300

    Métodos da classe

--- avancado/aluraframe/client/js/app/models/Negociacao.js
+++ avancado/aluraframe/client/js/app/models/Negociacao.js
@@ -1,7 +1,13 @@
 class Negociacao {
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
+
+    obterVolume() {
+        return this.quantidade * this.valor;
+    }
+
 }
