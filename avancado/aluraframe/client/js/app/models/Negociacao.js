e7603c362e0bea1f876ebf2fbf96cff84fb02a80
Rafael Barros <rafael.rph7@gmail.com>
2017-08-14 21:03:35 -0300

    get methods

--- avancado/aluraframe/client/js/app/models/Negociacao.js
+++ avancado/aluraframe/client/js/app/models/Negociacao.js
@@ -1,13 +1,28 @@
 class Negociacao {
     
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     obterVolume() {
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
     }
 
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
+    }
+
+    getVolume(){
+        return this._volume;
+    }
 }
