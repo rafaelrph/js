3a0a145685ce034bacbb5d67e6c5f9140fa0188f
Rafael Barros <rafael.rph7@gmail.com>
2017-08-14 21:05:17 -0300

    Get sintax

--- avancado/aluraframe/client/js/app/models/Negociacao.js
+++ avancado/aluraframe/client/js/app/models/Negociacao.js
@@ -10,19 +10,19 @@ class Negociacao {
         return this._quantidade * this._valor;
     }
 
-    getData() {
+    get data() {
         return this._data;
     }
 
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade;
     }
 
-    getValor() {
+    get valor() {
         return this._valor;
     }
 
-    getVolume(){
+    get volume(){
         return this._volume;
     }
 }
