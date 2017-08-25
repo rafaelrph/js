17c62ab470a60df4ba326ecb5cd750ab100431bd
Rafael Barros <rafael.rph7@gmail.com>
2017-08-25 09:36:23 -0300

    HttpService

--- avancado/aluraframe/client/js/app/services/NegociacaoService.js
+++ avancado/aluraframe/client/js/app/services/NegociacaoService.js
@@ -1,53 +1,30 @@
 class NegociacaoService {
-    
+	
+	constructor() {
+		this._http = new HttpService();
+	}
+
     obterNegociacoesDaSemana() {
 		return new Promise((resolve, reject) => {
-			let xhr = new XMLHttpRequest();
-			xhr.open('GET', 'negociacoes/semana');
-			xhr.onreadystatechange = () => {
-				if(xhr.readyState == 4) {
-					if(xhr.status == 200) {
-						resolve(JSON.parse(xhr.responseText).map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));                    
-					} else {
-						reject("Erro na comunicação com a API.");
-					}
-				}
-			};
-			xhr.send();
+			this._http.get('negociacoes/semana')
+			.then(negociacoes => resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))) 
+			.catch(erro => reject("Não foi possível buscar os registros da semana."))
 		});
 	}
 	
 	obterNegociacoesAnterior() {
 		return new Promise((resolve, reject) => {
-			let xhr = new XMLHttpRequest();
-			xhr.open('GET', 'negociacoes/anterior');
-			xhr.onreadystatechange = () => {
-				if(xhr.readyState == 4) {
-					if(xhr.status == 200) {
-						resolve(JSON.parse(xhr.responseText).map((objeto) => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-					} else {
-						reject("Erro na comunicação com a API.");
-					}
-				}
-			};
-			xhr.send();
+			this._http.get('negociacoes/anterior')
+			.then(negociacoes => resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))) 
+			.catch(erro => reject("Não foi possível buscar os registros da semana anterior."))
 		});
 	}
 
 	obterNegociacoesRetrasada() {
 		return new Promise((resolve, reject) => {
-			let xhr = new XMLHttpRequest();
-			xhr.open('GET', 'negociacoes/retrasada');
-			xhr.onreadystatechange = () => {
-				if(xhr.readyState == 4) {
-					if(xhr.status == 200) {
-						resolve(JSON.parse(xhr.responseText).map((objeto) => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-					} else {
-						reject("Erro na comunicação com a API.");
-					}	
-				}
-			};
-			xhr.send();
+			this._http.get('negociacoes/retrasada')
+			.then(negociacoes => resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))) 
+			.catch(erro => reject("Não foi possível buscar os registros da retrasada."))
 		});
 	}
 
